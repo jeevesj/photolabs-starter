f58752b1e4380d8924402ebcd378868861e965e7
Christian Gravel <cjgravel@gmail.com>
2023-05-23 00:49:04 -0700

    useReducer instead of useState, refactored useApplicationData

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,39 +1,60 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+export const ACTIONS = {
+  SET_SHOW_MODAL: 'SET_SHOW_MODAL',
+  SET_SELECTED_PHOTO: 'SET_SELECTED_PHOTO',
+  SET_FAV_PHOTOS: 'SET_FAV_PHOTOS',
+}
+
+function reducer(state, action) {
+  if (action.type === ACTIONS.SET_SHOW_MODAL) {
+    return { ...state, showModal: action.payload };
+  }
+  else if (action.type === ACTIONS.SET_SELECTED_PHOTO) {
+    return { ...state, selectedPhoto: action.payload };
+  }
+  else if (action.type === ACTIONS.SET_FAV_PHOTOS) {
+    return { ...state, favPhotos: action.payload };
+  }
+  else {
+    throw new Error(`Tried to reduce with unsupported action type: ${action.type}`);
+  }
+}
+
+const initialState = {
+  showModal: false,
+  selectedPhoto: null,
+  favPhotos: [],
+};
 
 export default function useApplicationData() {
-  const [showModal, setShowModal] = useState(false);
-  const [selectedPhoto, setSelectedPhoto] = useState(null);
-  const [favPhotos, setFavPhotos] = useState([]);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const handlePhotoClick = (photo) => {
-    setSelectedPhoto(photo);
-    setShowModal(true);
+    dispatch({ type: ACTIONS.SET_SELECTED_PHOTO, payload: photo });
+    dispatch({ type: ACTIONS.SET_SHOW_MODAL, payload: true });
   };
 
   const handleModalClose = () => {
-    setSelectedPhoto(null);
-    setShowModal(false);
+    dispatch({ type: ACTIONS.SET_SELECTED_PHOTO, payload: null });
+    dispatch({ type: ACTIONS.SET_SHOW_MODAL, payload: false });
   };
 
   const handleFavClick = (photoId) => {
-    let newFavPhotos = [...favPhotos];
-    if (favPhotos.includes(photoId)) {
+    let newFavPhotos = [...state.favPhotos];
+    if (state.favPhotos.includes(photoId)) {
       newFavPhotos = newFavPhotos.filter(id => id !== photoId);
     } else {
       newFavPhotos.push(photoId);
     }
-    setFavPhotos(newFavPhotos);
+    dispatch({ type: ACTIONS.SET_FAV_PHOTOS, payload: newFavPhotos });
     console.log("Number of favorite photos:", newFavPhotos.length);
   };
 
-
   return {
-    showModal,
-    selectedPhoto,
-    favPhotos,
+    ...state,
     handlePhotoClick,
     handleModalClose,
     handleFavClick,
-  
   };
 }
